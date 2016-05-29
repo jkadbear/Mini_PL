742ea9e59216ef866f41172d3299656099e54226
jkadbear <jkadbear@gmail.com>
2016-05-29 11:49:45 +0800

    add Closure && implement High Order Function

--- MiniInterpreter.py
+++ MiniInterpreter.py
@@ -42,9 +42,11 @@ Read      = 64
 Print     = 65
 Pro       = 71
 FunName   = 101
+
 term_kind = {'Decl': 11, 'Para': 21, 'Num': 31, 'Id': 32, 'AppFun': 33, 'Plus': 34, \
 		'Minus': 35, 'Mult': 36, 'List': 41, 'Func': 51, 'LetBe': 61, 'RunFun': 62, \
 		'Return': 63, 'Read': 64, 'Print': 65, 'Pro': 71, 'FunName': 101, 'End': None}
+
 
 class Node:
 	"""docstring for Node"""
@@ -54,36 +56,33 @@ class Node:
 		self.num         = None
 		self.ident       = None
 		self.child_nodes = []
+		self.closure     = None # used when constr_kind == FunName
 
-	def show_AST(self):
-		print('father: ' + str(self.kind))
-		for child in self.child_nodes:
-			print(child.kind)
-		for child in self.child_nodes:
-			child.show_AST()
+
+class Closure:
+	def __init__(self, access_link, func_node):
+		self.access_link = access_link
+		self.func_node   = func_node
+
 
 class Activity_Record:
 	"""docstring for Activity_Record"""
 	def __init__(self, access_link):
 		self.access_link = access_link
 		self.vars        = []
-		self.funcs       = []
+		self.closures    = []
 
-	def add_vars(self, ident):
+	def add_var(self, ident):
 		self.vars.append({'name': ident, 'value': 0})
 
-	def add_func(self, node):
-		# Func Node
-		self.funcs.append({'name': node.child_nodes[0].ident, 'node': node})
-
-	def set_vars(self, ident, num):
+	def set_var(self, ident, num):
 		var = next((iter for iter in self.vars if iter['name'] == ident), None)
 		if var != None:
 			var['value'] = num
 		elif self.access_link != None:
-			self.access_link.set_vars(ident, num)
+			self.access_link.set_var(ident, num)
 
-	def get_val(self, node):
+	def get_value(self, node):
 		# Id Node
 		if node.constr_kind == Id:
 			var = next((iter for iter in self.vars if iter['name'] == \
@@ -91,7 +90,7 @@ class Activity_Record:
 			if var != None:
 				return var['value']
 			elif self.access_link != None:
-				return self.access_link.get_val(node)
+				return self.access_link.get_value(node)
 			else:
 				return None
 		# Num Node or Print Node or Return Node
@@ -102,16 +101,30 @@ class Activity_Record:
 		else:
 			return node.num
 
-	def get_func(self, node):
-		# AppFun Node
-		fun = next((iter for iter in self.funcs if iter['name'] == \
-						 node.child_nodes[0].ident), None)
+	def add_closure(self, func_name, closure):
+		# Func Node
+		self.closures.append({'name': func_name, 'closure': closure})
+
+	def get_closure(self, func_name):
+		# AppFun Node or RunFun Node or FunName Node
+		fun = next((iter for iter in self.closures if iter['name'] == \
+						 func_name), None)
 		if fun != None:
-			return (fun['node'], self)
+			return fun['closure']
 		elif self.access_link != None:
-			return self.access_link.get_func(node)
+			return self.access_link.get_closure(func_name)
 		else:
-			return (None, None)
+			return None
+
+	def set_closure(self, func_name, closure):
+		# AppFun Node
+		fun = next((iter for iter in self.closures if iter['name'] == \
+						 func_name), None)
+		if fun != None:
+			fun['closure'] = closure
+		else:
+			self.closures.append({'name': func_name, 'closure': closure})
+
 
 class Interpreter:
 	"""
@@ -122,8 +135,8 @@ class Interpreter:
 		self.reader   = self.get_read()
 		self.root     = None
 		self.output   = []
-		self.AR_stk   = []
 		self.cur_AR   = Activity_Record(None)
+		self.AR_stk   = [self.cur_AR]
 		self.ret_val  = 0
 
 	def get_item(self):
@@ -166,16 +179,6 @@ class Interpreter:
 					term_stk.append(node)
 		self.root = term_stk[-1]
 
-	def run_node(self, AppNode, FunctionNode):
-		"""
-		run a function
-		"""
-		for L, P in zip(AppNode.child_nodes[1].child_nodes, FunctionNode.child_nodes[1].child_nodes):
-			self.add_vars(P.ident)
-			self.set_vars(P.ident, L.num)
-		self.interpret(FunctionNode.child_nodes[2])
-		return self.ret_val
-
 	def add_AR(self, access_link):
 		"""
 		add new activity record
@@ -183,20 +186,39 @@ class Interpreter:
 		self.cur_AR = Activity_Record(access_link)
 		self.AR_stk.append(self.cur_AR)
 
-	def add_vars(self, ident):
-		self.cur_AR.add_vars(ident)
+	def add_var(self, ident):
+		self.cur_AR.add_var(ident)
 
-	def set_vars(self, ident, num):
-		self.cur_AR.set_vars(ident, num)
+	def set_var(self, ident, num):
+		self.cur_AR.set_var(ident, num)
 
-	def get_val(self, node):
-		return self.cur_AR.get_val(node)
+	def get_value(self, node):
+		return self.cur_AR.get_value(node)
 
-	def add_func(self, node):
-		self.cur_AR.add_func(node)
+	def add_closure(self, func_name, node):
+		new_closure = Closure(self.cur_AR, node)
+		self.cur_AR.add_closure(func_name, new_closure)
 
-	def get_func(self, node):
-		return self.cur_AR.get_func(node)
+	def get_closure(self, func_name):
+		return self.cur_AR.get_closure(func_name)
+
+	def set_closure(self, func_name, closure):
+		self.cur_AR.set_closure(func_name, closure)
+
+	def run_node(self, appfun_node, func_node):
+		"""
+		run a function
+		"""
+		for L, P in zip(appfun_node.child_nodes[1].child_nodes, func_node.child_nodes[1].child_nodes):
+			if P.constr_kind == FunName:
+				self.set_closure(P.child_nodes[0].ident, L.closure)
+			else:
+				self.add_var(P.ident)
+				self.set_var(P.ident, L.num)
+		self.interpret(func_node.child_nodes[2])
+		self.cur_AR = self.AR_stk[-2]
+		self.AR_stk.pop()
+		return self.ret_val
 
 	def interpret(self, node):
 		"""
@@ -204,54 +226,57 @@ class Interpreter:
 		"""
 		self.ret_val = 0
 		if node.constr_kind == Id or node.constr_kind == Num:
-			node.num = self.get_val(node)
+			node.num = self.get_value(node)
 		elif node.constr_kind == Plus:
 			self.interpret(node.child_nodes[0])
 			self.interpret(node.child_nodes[1])
-			node.num = self.get_val(node.child_nodes[0]) + \
-						self.get_val(node.child_nodes[1])
+			node.num = self.get_value(node.child_nodes[0]) + \
+						self.get_value(node.child_nodes[1])
 		elif node.constr_kind == Minus:
 			self.interpret(node.child_nodes[0])
 			self.interpret(node.child_nodes[1])
-			node.num = self.get_val(node.child_nodes[0]) - \
-						self.get_val(node.child_nodes[1])
+			node.num = self.get_value(node.child_nodes[0]) - \
+						self.get_value(node.child_nodes[1])
 		elif node.constr_kind == Mult:
 			self.interpret(node.child_nodes[0])
 			self.interpret(node.child_nodes[1])
-			node.num = self.get_val(node.child_nodes[0]) * \
-						self.get_val(node.child_nodes[1])
+			node.num = self.get_value(node.child_nodes[0]) * \
+						self.get_value(node.child_nodes[1])
 		elif node.constr_kind == AppFun or node.constr_kind == RunFun:
 			for subnode in node.child_nodes[1].child_nodes:
 				self.interpret(subnode)
-			(function, access_link) = self.get_func(node)
-			self.add_AR(access_link)
-			node.num = self.run_node(node, function)
+			closure = self.get_closure(node.child_nodes[0].ident)
+			self.add_AR(closure.access_link)
+			node.num = self.run_node(node, closure.func_node)
 		elif node.constr_kind == Pro:
 			for subnode in node.child_nodes:
 				self.interpret(subnode)
 				if subnode.constr_kind == Return:
 					break
 		elif node.constr_kind == Func:
-			self.add_func(node)
+			self.add_closure(node.child_nodes[0].ident, node)
 		elif node.constr_kind == Decl:
 			for subnode in node.child_nodes:
-				self.add_vars(subnode.ident)
+				self.add_var(subnode.ident)
 		elif node.constr_kind == LetBe:
 			self.interpret(node.child_nodes[1])
-			self.set_vars(node.child_nodes[0].ident, self.get_val(node.child_nodes[1]))
+			self.set_var(node.child_nodes[0].ident, self.get_value(node.child_nodes[1]))
 		elif node.constr_kind == Read:
-			self.set_vars(node.child_nodes[0].ident, next(self.reader))
+			self.set_var(node.child_nodes[0].ident, next(self.reader))
 		elif node.constr_kind == Print:
 			self.interpret(node.child_nodes[0])
-			self.do_write(self.get_val(node))
+			self.do_write(self.get_value(node))
 		elif node.constr_kind == Return:
 			self.interpret(node.child_nodes[0])
-			self.ret_val = self.get_val(node)
+			self.ret_val = self.get_value(node)
+		elif node.constr_kind == FunName:
+			node.closure = self.get_closure(node.child_nodes[0].ident)
 
 	def final_write(self):
 		with open('output.txt', 'w') as f:
 			for out in self.output:
 				f.write(str(out) + ' ')
+
 
 if __name__ == '__main__':
 	args = docopt(__doc__, version='1.0')
@@ -265,4 +290,3 @@ if __name__ == '__main__':
 	my_interpreter.parse()
 	my_interpreter.interpret(my_interpreter.root)
 	my_interpreter.final_write()
-	# my_interpreter.root.show_AST()
